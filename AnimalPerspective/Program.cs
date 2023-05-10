ba94f1ff256b0b515fa3992f86a32a2bd695f2c8
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-05-10 10:05:07 +0100

    Changed Program from mine to teachers

--- AnimalPerspective/Program.cs
+++ AnimalPerspective/Program.cs
@@ -6,26 +6,48 @@ namespace AnimalPerspective
     {
         private static void Main()
         {
-            Animal animal1 = new Dog();
-            Animal animal2 = new Cat();
-            Animal animal3 = new Bat();
-            Animal animal4 = new Bee();
-            WriteAnimalInformation(animal1);
-            WriteAnimalInformation(animal2);
-            WriteAnimalInformation(animal3);
-            WriteAnimalInformation(animal4);
-        }
+            const int n = 10;
+            Animal[] animals = new Animal[n];
+            Random rnd = new Random();
 
-        private static void WriteAnimalInformation(Animal newAnimal)
-        {
-            if(newAnimal is ICanFly)
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine($"{newAnimal.GetType().Name} Number of wings = {((ICanFly)newAnimal).NumberOfWings}");
+                animals[i] = rnd.Next(4) switch
+                {
+                    0 => new Bat(),
+                    1 => new Bee(),
+                    2 => new Cat(),
+                    3 => new Dog(),
+                    _ => throw new ArgumentOutOfRangeException("Unknown animal!")
+                };
             }
-            if(newAnimal is IMammal)
+
+            foreach (Animal a in animals)
             {
-                Console.WriteLine($"{newAnimal.GetType().Name} Number of nipples = {((IMammal)newAnimal).NumberOfNipples}");
+                Console.Write($"{a.GetType().Name} says ");
+                Console.Write(a.Sound());
+                if (a is IMammal)
+                    Console.Write(
+                        $", No.Nipples = {(a as IMammal).NumberOfNipples}");
+                if (a is ICanFly)
+                    Console.Write(
+                        $", No.Wings = {(a as ICanFly).NumberOfWings}");
+                Console.WriteLine();
             }
+
+            // Possible output:
+
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Bat says Sound will be <Click>, No.Nipples = 2, No.Wings = 2
+            // Dog says Sound will be Woof!, No.Nipples = 8
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Cat says Sound will be Miau, No.Nipples = 6
+            // Dog says Sound will be Woof!, No.Nipples = 8
+
         }
     }
 }
